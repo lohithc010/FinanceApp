33f05e6f1ab80e3e1f800624a9ccecbcba835848
lohithc010 <lohithc010@gmail.com>
2022-06-15 11:16:14 +0530

    UI Update 3

---
+++ p3.py
@@ -0,0 +1,7 @@
+print('About Employee Details')
+print('---------------------------------')
+print(469259, type(469259))
+print('Employee Name : Lohith Chowdary',type('Employee Name : Lohith Chowdary'))
+print('Origination', type('Origination'))
+print(15000.00, type(15000.00))
+print('End the line')
