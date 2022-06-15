fbbedc6cf5afc91890a4beb0f6e009d855353fc7
lohithc010 <lohithc010@gmail.com>
2022-06-15 12:20:05 +0530

    2nd Step

---
+++ p4.py
@@ -0,0 +1,9 @@
+name = "Lohith"
+eid = 469259 
+base_pay = 10000
+tax = 12*base_pay/100
+sum = base_pay + tax
+print('''Name: {}
+      ID:{}
+      Base PAY:{}
+      Total pay:{}''' .format(name,eid,base_pay, int(sum)))
